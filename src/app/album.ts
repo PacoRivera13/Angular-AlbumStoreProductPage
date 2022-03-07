41d51161652c89a36bab03e5081d45d729e7a021
Francisco Rivera <fran.gori90@gmail.com>
2022-03-07 13:52:48 -0600

    Adding A Type To The getAlbum JSON Response

--- src/app/album.ts
+++ src/app/album.ts
@@ -2,7 +2,7 @@ import { Track } from "./track";
 
 export interface Album {
     name: string;
-    releaseDate: Date;
+    releaseDate: string;
     coverImage: string;    
     tracks: Track[];
 }
